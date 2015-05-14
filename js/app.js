5c0c4e1318c7d31f513158dd5cc51d612b49c7ee
Petar Petkov <petar.b.petkov@gmail.com>
2015-05-15 02:56:07 +0300

    Added Authentication controller and routing in app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-var app = angular.module('fakeBook', []);
+'use strict';
+
+var app = angular.module('fakeBook', ['ngRoute']);
+
+app.constant('baseServiceUrl', 'http://softuni-social-network.azurewebsites.net/api/');
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/login', {
+            templateUrl: 'partials/login.html',
+            controller: 'publicController'
+        })
+        .when('/register', {
+            templateUrl: 'partials/register.html',
+            controller: 'publicController'
+        })
+        .when('/', {
+            templateUrl: 'partials/home.html',
+            controller: 'publicController'
+        })
+        .otherwise({ redirectTo: '/' });
+});
+
---
+++ js/controllers/AuthenticationController.js
@@ -0,0 +1,43 @@
+﻿'use strict';
+
+app.controller('authenticationController', function ($scope, $location, $route, authentication, notifyService) {
+
+    var ClearData = function () {
+        $scope.loginData = "";
+        $scope.registerData = "";
+        $scope.userData = "";
+        $scope.passwordData = "";
+    };
+
+    $scope.login = function () {
+        authentication.Login($scope.loginData, function (serverData) {
+            notifyService.showInfo("Successful Login!");
+            authentication.SetCredentials(serverData);
+            ClearData();
+            $location.path('/home');
+        },
+        function (serverError) {
+            notifyService.showError("Unsuccessful Login!", serverError);
+        });
+    };
+
+    $scope.register = function () {
+        authentication.Register($scope.registerData, function (serverData) {
+            notifyService.showInfo("Successful Registeration!");
+            authentication.SetCredentials(serverData);
+            ClearData();
+            $location.path('/home');
+        },
+        function (serverError) {
+            notifyService.showError("Unsuccessful Registeration!", serverError);
+        });
+    };
+
+    $scope.logout = function () {
+        notifyService.showInfo("Successful Logout!");
+        ClearData();
+        authentication.ClearCredentials();
+        mainData.clearParams();
+        $route.reload();
+    };
+});
