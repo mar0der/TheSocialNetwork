10c79e302cdb959a8526903d5ebdf14128489835
Petar Petkov <petar.b.petkov@gmail.com>
2015-05-25 22:16:52 +0300

    Friends side bar dinamyc links to full friends page added

--- js/controllers/friendsSidebarController.js
+++ js/controllers/friendsSidebarController.js
@@ -1,21 +1,22 @@
 ﻿'use strict';
 
-app.controller('friendsSidebarController', function ($scope, $routeParams, profileService, usersService) {
+app.controller('friendsSidebarController', function ($scope, $routeParams,authenticationService, profileService, usersService) {
     $scope.showFriends = false;
 
-    var username = $routeParams.username;
+    $scope.username = $routeParams.username;
 
     if ($scope.isMe()) {
         profileService.getOwnFriendsPreview()
             .then(function (responseData) {
                 $scope.data = responseData;
+                $scope.username = authenticationService.getUsername();
                 $scope.showFriends = true;
             });
     } else {
-        usersService.getUserPreviewData(username)
+        usersService.getUserPreviewData($scope.username)
             .then(function (responseData) {
                 if (responseData.isFriend) {
-                    usersService.getFriendsFriendListPreview(username)
+                    usersService.getFriendsFriendListPreview($scope.username)
                         .then(function (friendData) {
                             $scope.showFriends = true;
                             $scope.data = friendData;
