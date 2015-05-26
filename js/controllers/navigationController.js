26937f12621de3b6b7db8db20d6314bf18923302
Petar Petkov <petar.b.petkov@gmail.com>
2015-05-27 01:09:03 +0300

    Search for users added

--- js/controllers/navigationController.js
+++ js/controllers/navigationController.js
@@ -1,8 +1,10 @@
 ﻿'use strict';
 
-app.controller('navigationController', function ($scope, $location, $interval, profileService, notyService) {
+app.controller('navigationController', function ($scope, $location, $timeout, $interval, profileService, usersService, notyService) {
 
     $scope.showNotification = false;
+    $scope.showSearchResults = false;
+    $scope.searchPattern = '';
 
     //uncomment when ready for testing
     function refreshPendingRequests() {
@@ -15,18 +17,45 @@ app.controller('navigationController', function ($scope, $location, $interval, p
                 } else {
                     $scope.showNotification = false;
                 }
-            }, function(serverError) {
+            }, function (serverError) {
                 notyService.showError("Can`t pull your friends requests", serverError);
             });
     }
 
-    //refreshPendingRequests();
+    $scope.searchByUsername = function searchByUsername() {
+        if ($scope.searchPattern !== '') {
+            usersService.searchUserByName($scope.searchPattern)
+                .then(function(responseData) {
+                    if (responseData.length) {
+                        $scope.searchResults = responseData;
+                        $scope.showSearchResults = true;
+                    } else {
+                        $scope.showSearchResults = false;
+                    }
+                }, function(serverError) {
+                    $scope.showSearchResults = false;
+                    notyService.showError("An error occured while searching...", serverError);
+                });
+        } else {
+            $scope.showSearchResults = false;
+            $scope.searchResults = [];
+        }
+    }
+
+    $scope.hideSearchForm = function hideSearchForm() {
+        $timeout(function () {
+            $scope.showSearchResults = false;
+            $scope.searchResults = [];
+            $scope.searchPattern = '';
+        }, 400);
+
+    }
 
     var requestsInterval = $interval(function () {
         refreshPendingRequests();
-    }, 5000);
+    }, 60000);
 
-    //$scope.$on('$destroy', function () {
-    //    $interval.cancel(requestsInterval);
-    //});
+    $scope.$on('$destroy', function () {
+        $interval.cancel(requestsInterval);
+    });
 });
