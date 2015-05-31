ece5a9ff963138d2fdcc98f421152ad272af7bf5
Petar Petkov <petar.b.petkov@gmail.com>
2015-05-31 03:36:01 +0300

    Untracked files added

---
+++ js/directives/customOnChangeDirective.js
@@ -0,0 +1,9 @@
+﻿app.directive('customOnChangeDirective', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var onChangeHandler = scope.$eval(attrs.customOnChangeDirective);
+            element.bind('change', onChangeHandler);
+        }
+    };
+});
---
+++ js/services/httpResponseInterceptorService.js
@@ -0,0 +1,24 @@
+﻿app.factory('httpResponseInterceptorService', ['$q', '$location', function ($q, $location) {
+    return {
+        response: function (response) {
+            if (response.status === 401) {
+                $location.path('/');
+            }
+            return response || $q.when(response);
+        },
+        responseError: function (rejection) {
+            if (rejection.status === 400) {
+                $location.path('/');
+            } else if (rejection.status === 401) {
+                //alert('410');
+
+                localStorage.removeItem('accessToken');
+                localStorage.removeItem('username');
+                $location.path('/welcome');
+            } else if (rejection.status === 404) {
+                $location.path('/404/');
+            }
+            return $q.reject(rejection);
+        }
+    }
+}]);
